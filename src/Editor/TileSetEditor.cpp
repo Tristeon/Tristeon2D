5b5c973d3d710f9bed44d114cd73bd6c565485bd
Leon Brands <lajbrands@outlook.com>
2020-05-19 14:20:45 +0200

    Added demo assets

--- src/Editor/TileSetEditor.cpp
+++ src/Editor/TileSetEditor.cpp
@@ -6,22 +6,22 @@ namespace TristeonEditor
 {
 	void TileSetEditor::initialize()
 	{
-		contents = findChild<QWidget*>("tilesets_contents");
-		tileSetTabs = new QTabWidget(contents);
-		tileSetTabs->setStyleSheet("background-color: rgb(255, 255, 255);\nborder-color: rgb(0, 0, 0);\ncolor: rgb(00, 00, 00);");
-		
-		std::string path = (QDir::currentPath() + "/Project/TilesetTest.tileset").toStdString();
-		auto testTab = new TileSetTab(path);
-		
-		std::string path2 = (QDir::currentPath() + "/Project/GoldBricks.tileset").toStdString();
-		auto testTab2 = new TileSetTab(path2);
-		
-		tileSetTabs->addTab(testTab,"TilesetTest");
-		tileSetTabs->addTab(testTab2,"Gold Bricks");
+		//contents = findChild<QWidget*>("tilesets_contents");
+		//tileSetTabs = new QTabWidget(contents);
+		//tileSetTabs->setStyleSheet("background-color: rgb(255, 255, 255);\nborder-color: rgb(0, 0, 0);\ncolor: rgb(00, 00, 00);");
+		//
+		//std::string path = (QDir::currentPath() + "/Project/TilesetTest.tileset").toStdString();
+		//auto testTab = new TileSetTab(path);
+		//
+		//std::string path2 = (QDir::currentPath() + "/Project/GoldBricks.tileset").toStdString();
+		//auto testTab2 = new TileSetTab(path2);
+		//
+		//tileSetTabs->addTab(testTab,"TilesetTest");
+		//tileSetTabs->addTab(testTab2,"Gold Bricks");
 
-		auto layout = new QVBoxLayout();
-		layout->addWidget(tileSetTabs);
-		contents->setLayout(layout);
+		//auto layout = new QVBoxLayout();
+		//layout->addWidget(tileSetTabs);
+		//contents->setLayout(layout);
 	}
 
 	void TileSetEditor::sceneLoaded(Tristeon::Scene* scene)
